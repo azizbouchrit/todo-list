37c22bcbe24fee5ef2ff3847990f3d214a93dfea
azizbouchrit <medazizbouchrit@gmail.com>
2020-04-06 16:37:34 +0200

    Updates

---
+++ precache-manifest.36558bb188f16d84eb400cfada274e52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913b82f1651c5c1c6acd33fec337a39b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e3730244686fca22c75",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ed0d7f204e9236c17af9",
+    "url": "/static/css/main.66bd7df0.chunk.css"
+  },
+  {
+    "revision": "0e3730244686fca22c75",
+    "url": "/static/js/2.fa008d8f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.fa008d8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0d7f204e9236c17af9",
+    "url": "/static/js/main.55464eca.chunk.js"
+  },
+  {
+    "revision": "090fbb06d804952982c0",
+    "url": "/static/js/runtime-main.3f054f4f.js"
+  }
+]);
